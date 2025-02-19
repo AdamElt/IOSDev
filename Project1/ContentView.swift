40d3e316166594ecd9cb9b88fe766f84747ed46f
adamel-telbani <eltelbani9@gmail.com>
2025-02-19 18:15:09 -0500

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Adam El-Telbani on 2/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
